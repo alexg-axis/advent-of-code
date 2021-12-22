0dd929553e27e5593b4cc8e3ccacc09ac5c2d6c2
Alex Gustafsson <alexg@axis.com>
2021-12-22 08:56:04 +0100

    Add solution to 2021/12/22 part 2

--- 2021/22/solutions.ts
+++ 2021/22/solutions.ts
@@ -2,6 +2,10 @@ import { Input } from "../../utils/deno/input.ts";
 
 type RebootStep = {
   on: boolean,
+  cube: Cube,
+};
+
+export type Cube = {
   minX: number,
   maxX: number,
   minY: number,
@@ -9,8 +13,6 @@ type RebootStep = {
   minZ: number,
   maxZ: number,
 };
-
-type Cube = {[key: number]: {[key: number]: {[key: number]: boolean}}};
 
 export function parseInput(input: Input): RebootStep[] {
   // on x=-48..-2,y=-16..31,z=-3..41
@@ -20,50 +22,68 @@ export function parseInput(input: Input): RebootStep[] {
     const numbers = parts[1].split(",").map(x => x.slice(2)).map(x => x.split("..")).flat().map(Number);
     return {
       on,
-      minX: numbers[0],
-      maxX: numbers[1],
-      minY: numbers[2],
-      maxY: numbers[3],
-      minZ: numbers[4],
-      maxZ: numbers[5],
+      cube: {
+        minX: numbers[0],
+        maxX: numbers[1],
+        minY: numbers[2],
+        maxY: numbers[3],
+        minZ: numbers[4],
+        maxZ: numbers[5],
+      },
     }
   });
 }
 
-export function solvePart1(steps: RebootStep[]): number {
-  // steps = steps.map(x => ({
-  //   on: x.on,
-  //   minX: Math.max(x.minX, -50),
-  //   minY: Math.max(x.minY, -50),
-  //   minZ: Math.max(x.minZ, -50),
-  //   maxX: Math.min(x.maxX, 50),
-  //   maxY: Math.min(x.maxY, 50),
-  //   maxZ: Math.min(x.maxZ, 50),
-  // }))
+export function getIntersection(a: Cube, b: Cube): Cube | null {
+  // on x=-5..47,y=-31..22,z=-19..33
+  // on x = -44..5, y = -27..21, z = -14..35
+  const minX = Math.max(a.minX, b.minX);
+  const maxX = Math.min(a.maxX, b.maxX);
+  const minY = Math.max(a.minY, b.minY);
+  const maxY = Math.min(a.maxY, b.maxY);
+  const minZ = Math.max(a.minZ, b.minZ);
+  const maxZ = Math.min(a.maxZ, b.maxZ);
+  if (minX > maxX || minY > maxY || minZ > maxZ)
+    return null;
+  return { minX, maxX, minY, maxY, minZ, maxZ };
+}
 
-  steps = steps.filter(x => x.minX >= -50 && x.minY >= -50 && x.minZ >= -50 && x.maxX <= 50 && x.maxY <= 50 && x.maxZ <= 50);
+function solve(steps: RebootStep[]): number {
+  const intersections: RebootStep[] = [];
 
-  const cube: Cube = {};
-  let count = 0;
-  for (const {on, minX, maxX, minY, maxY, minZ, maxZ} of steps) {
-    // console.log(on, minX, maxX, minY, maxY, minZ, maxZ);
-    for (let x = minX; x <= maxX; x++) {
-      if (typeof cube[x] === "undefined")
-        cube[x] = {};
-      for (let y = minY; y <= maxY; y++) {
-        if (typeof cube[x][y] === "undefined")
-          cube[x][y] = {};
-        for (let z = minZ; z <= maxZ; z++) {
-          if (!cube[x][y][z] && on)
-            count++;
-          if (cube[x][y][z] && !on)
-            count--;
-          cube[x][y][z] = on;
-        }
-      }
+  for (const { on, cube } of steps) {
+    // Cancel out the effect of any previous cube
+    const length = intersections.length;
+    for (let i = 0; i < length; i++) {
+      const other = intersections[i];
+      const intersection = getIntersection(other.cube, cube);
+      if (intersection === null)
+        continue;
+
+      intersections.push({ on: !other.on, cube: intersection });
     }
-    // console.log(count);
+    if (on)
+      intersections.push({ on, cube });
+  }
+
+  let count = 0;
+  for (const { on, cube: { minX, maxX, minY, maxY, minZ, maxZ } } of intersections) {
+    const volume = (maxX - minX + 1) * (maxY - minY + 1) * (maxZ - minZ + 1);
+    if (on)
+      count += volume;
+    else
+      count -= volume;
   }
 
   return count;
+}
+
+export function solvePart1(steps: RebootStep[]): number {
+  steps = steps.filter(x => x.cube.minX >= -50 && x.cube.minY >= -50 && x.cube.minZ >= -50 && x.cube.maxX <= 50 && x.cube.maxY <= 50 && x.cube.maxZ <= 50);
+
+  return solve(steps);
+}
+
+export function solvePart2(steps: RebootStep[]): number {
+  return solve(steps);
 }
