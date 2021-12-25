59292110a91b2a39b68b7dc2b1b092764c718d8c
Alex Gustafsson <alexg@axis.com>
2021-12-25 11:16:17 +0100

    Pad the day of directories

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,8 @@ set -e
 year="${AOC_YEAR:-$(date +%Y)}"
 month="${AOC_MONTH:-$(date +%m)}"
 day="${AOC_DAY:-$(date +%-d)}"
+padded_day="0$day"
+padded_day="${padded_day: -2}"
 
 if [[ ! "$month" -eq 12 ]]; then
   echo "It's not december quite yet. If you wish to access previous events"
@@ -12,11 +14,11 @@ if [[ ! "$month" -eq 12 ]]; then
   exit 1
 fi
 
-mkdir -p "$year/$day"
+mkdir -p "$year/$padded_day"
 
 export PATH="/usr/local/opt/python@3.10/bin:$PATH"
 session_cookie="$(security find-generic-password -w -a "com.adventofcode.SessionCookie")"
-curl --silent --cookie "session=$session_cookie" "https://adventofcode.com/$year/day/$day" | ./scripts/html2md.py > "$year/$day/README.md"
-curl --silent --cookie "session=$session_cookie" "https://adventofcode.com/$year/day/$day/input" > "$year/$day/input.txt"
+curl --silent --cookie "session=$session_cookie" "https://adventofcode.com/$year/day/$day" | ./scripts/html2md.py > "$year/$padded_day/README.md"
+curl --silent --cookie "session=$session_cookie" "https://adventofcode.com/$year/day/$day/input" > "$year/$padded_day/input.txt"
 
-echo "$day/12 $year initialized successfully in ./$year/$day" >&2
+echo "$day/12 $year initialized successfully in ./$year/$padded_day" >&2
