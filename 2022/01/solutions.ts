4256f522a4044b3f4bbadfcc722c30128707909d
Alex Gustafsson <alexg@axis.com>
2022-12-01 08:30:24 +0100

    Add solution to 2022/12/01 part 2

--- 2022/01/solutions.ts
+++ 2022/01/solutions.ts
@@ -8,3 +8,9 @@ export function solvePart1(input: Input): number {
   const rations = input.raw.split("\n\n").map((x) => x.split("\n").map(Number));
   return rations.sort((a, b) => sum(b) - sum(a)).map(sum)[0];
 }
+
+export function solvePart2(input: Input): number {
+  const rations = input.raw.split("\n\n").map((x) => x.split("\n").map(Number));
+  const sorted = rations.sort((a, b) => sum(b) - sum(a)).map(sum);
+  return sum(sorted.slice(0, 3));
+}
--- 2022/01/test.ts
+++ 2022/01/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `1000
@@ -21,4 +21,24 @@ Deno.test("part 1 - given test case", () => {
 
   const max = solvePart1(new Input(input));
   assertEquals(max, 24000);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+  const max = solvePart2(new Input(input));
+  assertEquals(max, 45000);
 });
