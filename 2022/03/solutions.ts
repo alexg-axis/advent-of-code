535ea5f20880d6989577d721c7922884fd0ccab1
Alex Gustafsson <alexg@axis.com>
2022-12-03 08:49:41 +0100

    Use array utils

--- 2022/03/solutions.ts
+++ 2022/03/solutions.ts
@@ -1,3 +1,4 @@
+import { chunks, sum } from "../../utils/deno/arrays.ts";
 import type { Input } from "../../utils/deno/input.ts";
 
 export function priority(c: string): number {
@@ -21,13 +22,6 @@ function inGroup(a: string[], b: string[], c: string[]): string {
   return a.filter((x) => b.includes(x)).filter((x) => c.includes(x))[0];
 }
 
-function chunks<T>(x: T[], length: number): T[][] {
-  return Array(Math.ceil(x.length / length))
-    .fill(undefined)
-    .map((_, index) => index * length)
-    .map((begin) => x.slice(begin, begin + length));
-}
-
 export function solvePart1(input: Input): number {
   return input.lines
     .map(
@@ -38,14 +32,13 @@ export function solvePart1(input: Input): number {
         ] as [string[], string[]]
     )
     .map((x) => priority(inBoth(...x)))
-    .reduce((sum, x) => sum + x, 0);
+    .reduce(sum);
 }
 
 export function solvePart2(input: Input): number {
-  return chunks(
-    input.lines.map((x) => x.split("")),
-    3
-  )
+  return input.lines
+    .map((x) => x.split(""))
+    .reduce(chunks(3), [])
     .map((x) => priority(inGroup(x[0], x[1], x[2])))
-    .reduce((sum, x) => sum + x, 0);
+    .reduce(sum);
 }
