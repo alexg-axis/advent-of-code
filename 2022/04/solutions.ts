1ed25714ff2c88505f4407d8b4a0dc50a8ae639b
Alex Gustafsson <alexg@axis.com>
2022-12-04 07:34:03 +0100

    Add solution to 2022/12/04 part 2

--- 2022/04/solutions.ts
+++ 2022/04/solutions.ts
@@ -4,8 +4,23 @@ function oneFullyContainsTheOther(a: number[], b: number[]): boolean {
   return (a[0] >= b[0] && a[1] <= b[1]) || (b[0] >= a[0] && b[1] <= a[1]);
 }
 
+export function explodeRange(range: string): Set<number> {
+  const [start, end] = range.split("-").map(Number);
+  return new Set(new Array(end - start + 1).fill(0).map((_, i) => start + i));
+}
+
+function setOverlaps(a: Set<number>, b: Set<number>): boolean {
+  return new Set([...a, ...b]).size !== a.size + b.size;
+}
+
 export function solvePart1(input: Input): number {
   return input.lines
     .map((line) => line.split(",").map((range) => range.split("-").map(Number)))
     .filter(([a, b]) => oneFullyContainsTheOther(a, b)).length;
+}
+
+export function solvePart2(input: Input): number {
+  return input.lines
+    .map((line) => line.split(",").map(explodeRange))
+    .filter(([a, b]) => setOverlaps(a, b)).length;
 }
--- 2022/04/test.ts
+++ 2022/04/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { explodeRange, solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `2-4,6-8
@@ -12,4 +12,19 @@ Deno.test("part 1 - given test case", () => {
 2-6,4-8`;
 
   assertEquals(solvePart1(new Input(input)), 2);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`;
+
+  assertEquals(solvePart2(new Input(input)), 4);
+});
+
+Deno.test("part 2 - explodeRange", () => {
+  assertEquals(explodeRange("3-7"), new Set([3, 4, 5, 6, 7]));
 });
