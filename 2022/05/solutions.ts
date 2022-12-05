69397021a8b677ed3e056b863a07bde937b8b16d
Alex Gustafsson <alexg@axis.com>
2022-12-05 07:51:00 +0100

    Add solution to 2022/12/05 part 2

--- 2022/05/solutions.ts
+++ 2022/05/solutions.ts
@@ -15,15 +15,48 @@ export function solvePart1(input: Input): string {
     columns.push(column);
   }
 
-  const movements = instructions.split("\n").map((x) => {
-    const parts = x.split(" ");
-    return [Number(parts[1]), Number(parts[3]) - 1, Number(parts[5]) - 1];
-  });
+  const movements = instructions
+    .split("\n")
+    .filter((x) => x)
+    .map((x) => {
+      const parts = x.split(" ");
+      return [Number(parts[1]), Number(parts[3]) - 1, Number(parts[5]) - 1];
+    });
 
   for (const [count, from, to] of movements) {
     for (let i = 0; i < count; i++) {
       columns[to].unshift(columns[from].shift()!);
     }
+  }
+
+  return columns.map((x) => x[0]).join("");
+}
+
+export function solvePart2(input: Input): string {
+  const [rawStacks, instructions] = input.raw.split("\n\n");
+
+  const lines = rawStacks.split("\n");
+  const columns: string[][] = [];
+  for (let i = 1; i < lines.at(-1)!.length; i += 4) {
+    const column: string[] = [];
+    for (let k = 0; k < lines.length - 1; k++) {
+      if (lines[k][i] && lines[k][i] !== " ") {
+        column.push(lines[k][i]);
+      }
+    }
+    columns.push(column);
+  }
+
+  const movements = instructions
+    .split("\n")
+    .filter((x) => x)
+    .map((x) => {
+      const parts = x.split(" ");
+      return [Number(parts[1]), Number(parts[3]) - 1, Number(parts[5]) - 1];
+    });
+
+  for (const [count, from, to] of movements) {
+    columns[to].unshift(...columns[from].splice(0, count));
   }
 
   return columns.map((x) => x[0]).join("");
--- 2022/05/test.ts
+++ 2022/05/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `    [D]
@@ -15,4 +15,36 @@ move 2 from 2 to 1
 move 1 from 1 to 2`;
 
   assertEquals(solvePart1(new Input(input)), "CMZ");
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+  assertEquals(solvePart2(new Input(input)), "MCD");
+});
+
+Deno.test("part 2 - partial input", () => {
+  const input = `                    [L]     [H] [W]
+                [J] [Z] [J] [Q] [Q]
+[S]             [M] [C] [T] [F] [B]
+[P]     [H]     [B] [D] [G] [B] [P]
+[W]     [L] [D] [D] [J] [W] [T] [C]
+[N] [T] [R] [T] [T] [T] [M] [M] [G]
+[J] [S] [Q] [S] [Z] [W] [P] [G] [D]
+[Z] [G] [V] [V] [Q] [M] [L] [N] [R]
+ 1   2   3   4   5   6   7   8   9
+
+move 1 from 3 to 5
+move 2 from 2 to 8
+move 4 from 1 to 3`;
+
+  assertEquals(solvePart2(new Input(input)), "JGSDHLJTW");
 });
