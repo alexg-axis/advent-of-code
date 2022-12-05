182f18598a4053dfe40ad6176e65e2f0feb863cc
Alex Gustafsson <alexg@axis.com>
2022-12-05 07:28:48 +0100

    Add solution to 2022/12/05 part 1

---
+++ 2022/05/solutions.ts
@@ -0,0 +1,30 @@
+import type { Input } from "../../utils/deno/input.ts";
+
+export function solvePart1(input: Input): string {
+  const [rawStacks, instructions] = input.raw.split("\n\n");
+
+  const lines = rawStacks.split("\n");
+  const columns: string[][] = [];
+  for (let i = 1; i < lines.at(-1)!.length; i += 4) {
+    const column: string[] = [];
+    for (let k = 0; k < lines.length - 1; k++) {
+      if (lines[k][i] && lines[k][i] !== " ") {
+        column.push(lines[k][i]);
+      }
+    }
+    columns.push(column);
+  }
+
+  const movements = instructions.split("\n").map((x) => {
+    const parts = x.split(" ");
+    return [Number(parts[1]), Number(parts[3]) - 1, Number(parts[5]) - 1];
+  });
+
+  for (const [count, from, to] of movements) {
+    for (let i = 0; i < count; i++) {
+      columns[to].unshift(columns[from].shift()!);
+    }
+  }
+
+  return columns.map((x) => x[0]).join("");
+}
---
+++ 2022/05/test.ts
@@ -0,0 +1,18 @@
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { Input } from "../../utils/deno/input.ts";
+
+import { solvePart1 } from "./solutions.ts";
+
+Deno.test("part 1 - given test case", () => {
+  const input = `    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+  assertEquals(solvePart1(new Input(input)), "CMZ");
+});
