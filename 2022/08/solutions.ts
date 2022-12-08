7f8dd32d3c331733a9117c129e3f6faf12d907b1
Alex Gustafsson <alexg@axis.com>
2022-12-08 07:19:40 +0100

    Add solution to 2022/12/08 part 2

--- 2022/08/solutions.ts
+++ 2022/08/solutions.ts
@@ -1,18 +1,23 @@
 import type { Input } from "../../utils/deno/input.ts";
 
-export function solvePart1(input: Input): number {
-  const grid = input.lines.map((x) => x.split("").map(Number));
+type Grid<T> = T[][];
 
-  let visible = 0;
+function parseGrid(input: Input): Grid<number> {
+  return input.lines.map((x) => x.split("").map(Number));
+}
+
+function viewingDistances(
+  grid: Grid<number>,
+  inclusive = false
+): Grid<[number, number, number, number]> {
+  const distances: Grid<[number, number, number, number]> = new Array(
+    grid.length
+  )
+    .fill(0)
+    .map(() => new Array(grid[0].length).fill(0).map(() => [0, 0, 0, 0]));
+
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[0].length; x++) {
-      const isEdge =
-        y === 0 || y === grid.length - 1 || x === 0 || x === grid[0].length - 1;
-      if (isEdge) {
-        visible++;
-        continue;
-      }
-
       // Above
       let visibleAbove = 0;
       for (
@@ -20,6 +25,10 @@ export function solvePart1(input: Input): number {
         y2 >= 0 && grid[y2][x] < grid[y][x];
         y2--, visibleAbove++
       );
+      distances[y][x][0] = visibleAbove;
+      if (inclusive && y - visibleAbove !== 0) {
+        distances[y][x][0]++;
+      }
 
       // Left
       let visibleLeft = 0;
@@ -28,6 +37,10 @@ export function solvePart1(input: Input): number {
         x2 >= 0 && grid[y][x2] < grid[y][x];
         x2--, visibleLeft++
       );
+      distances[y][x][1] = visibleLeft;
+      if (inclusive && x - visibleLeft !== 0) {
+        distances[y][x][1]++;
+      }
 
       // Left
       let visibleRight = 0;
@@ -36,6 +49,10 @@ export function solvePart1(input: Input): number {
         x2 < grid[0].length && grid[y][x2] < grid[y][x];
         x2++, visibleRight++
       );
+      distances[y][x][2] = visibleRight;
+      if (inclusive && x + visibleRight !== grid[0].length - 1) {
+        distances[y][x][2]++;
+      }
 
       // Undler
       let visibleUnder = 0;
@@ -44,12 +61,30 @@ export function solvePart1(input: Input): number {
         y2 < grid.length && grid[y2][x] < grid[y][x];
         y2++, visibleUnder++
       );
+      distances[y][x][3] = visibleUnder;
+      if (inclusive && y + visibleUnder !== grid.length - 1) {
+        distances[y][x][3]++;
+      }
+    }
+  }
 
+  return distances;
+}
+
+export function solvePart1(input: Input): number {
+  const grid = parseGrid(input);
+  const distances = viewingDistances(grid);
+
+  let visible = 0;
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[0].length; x++) {
+      const [visibleAbove, visibleLeft, visibleRight, visibleUnder] =
+        distances[y][x];
       if (
         visibleAbove === y ||
-        visibleUnder === grid.length - (y + 1) ||
         visibleLeft === x ||
-        visibleRight === grid[0].length - (x + 1)
+        visibleRight === grid[0].length - (x + 1) ||
+        visibleUnder === grid.length - (y + 1)
       ) {
         visible++;
       }
@@ -57,4 +92,22 @@ export function solvePart1(input: Input): number {
   }
 
   return visible;
+}
+
+export function solvePart2(input: Input): number {
+  const grid = parseGrid(input);
+  const distances = viewingDistances(grid, true);
+
+  let maxScore = 0;
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[0].length; x++) {
+      const score = distances[y][x].reduce((product, x) => product * x, 1);
+      if (score > maxScore) {
+        maxScore = score;
+      }
+    }
+  }
+
+  return maxScore;
 }
--- 2022/08/test.ts
+++ 2022/08/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `30373
@@ -10,4 +10,13 @@ Deno.test("part 1 - given test case", () => {
 33549
 35390`;
   assertEquals(solvePart1(new Input(input)), 21);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `30373
+25512
+65332
+33549
+35390`;
+  assertEquals(solvePart2(new Input(input)), 8);
 });
