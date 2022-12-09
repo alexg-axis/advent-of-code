80ae0382162ea64933556923e93ad306b878cf5b
Alex Gustafsson <alexg@axis.com>
2022-12-09 17:55:09 +0100

    Add solution to 2022/12/09 part 2

--- 2022/09/solutions.ts
+++ 2022/09/solutions.ts
@@ -38,6 +38,48 @@ function moveInDirection(coordinate: Vector, direction: Direction) {
   }
 }
 
+function replaceAt(string: string, index: number, replacement: string): string {
+  return (
+    string.substring(0, index) +
+    replacement +
+    string.substring(index + replacement.length)
+  );
+}
+
+function followHead(head: Vector, tail: Vector, direction: Direction) {
+  if (touching(head, tail)) {
+    return;
+  }
+
+  const d = distance(head, tail);
+  if (Math.abs(d.x) <= 2 && d.y === 0) {
+    moveInDirection(tail, d.x < 0 ? "R" : "L");
+  } else if (Math.abs(d.x) === 0 && d.y <= 2) {
+    moveInDirection(tail, d.y < 0 ? "U" : "D");
+  } else {
+    moveInDirection(tail, d.x < 0 ? "R" : "L");
+    moveInDirection(tail, d.y < 0 ? "U" : "D");
+  }
+}
+
+function display(vectors: Vector[], size: Vector, start: Vector) {
+  // Deep copy
+  vectors = [...vectors.map(({ x, y }) => ({ x, y }))];
+
+  for (const vector of vectors) {
+    vector.x += start.x;
+    vector.y += start.y;
+  }
+
+  const grid: string[] = new Array(size.y).fill(".".repeat(size.x));
+  for (let i = vectors.length - 1; i >= 0; i--) {
+    const { x, y } = vectors[i];
+    grid[y] = replaceAt(grid[y], x, i === 0 ? "H" : i.toString());
+  }
+  grid[start.y] = replaceAt(grid[start.y], start.x, "S");
+  console.log(grid.reverse().join("\n"));
+}
+
 export function solvePart1(input: Input): number {
   const moves: [Direction, number][] = input.lines.map((x) => {
     const parts = x.split(" ");
@@ -53,24 +95,42 @@ export function solvePart1(input: Input): number {
   for (const [direction, steps] of moves) {
     for (let i = 0; i < steps; i++) {
       moveInDirection(head, direction);
-
-      if (touching(head, tail)) {
-        continue;
-      }
-
-      const d = distance(head, tail);
-      if (
-        (Math.abs(d.x) <= 2 && d.y === 0) ||
-        (Math.abs(d.x) === 0 && d.y <= 2)
-      ) {
-        moveInDirection(tail, direction);
-      } else {
-        moveInDirection(tail, d.x < 0 ? "R" : "L");
-        moveInDirection(tail, d.y < 0 ? "U" : "D");
-      }
-
+      followHead(head, tail, direction);
       visited.add(`${tail.x}:${tail.y}`);
     }
+  }
+
+  return visited.size;
+}
+
+export function solvePart2(input: Input): number {
+  const moves: [Direction, number][] = input.lines.map((x) => {
+    const parts = x.split(" ");
+    return [parts[0] as Direction, Number(parts[1])];
+  });
+
+  const knots: Vector[] = new Array(10).fill(0).map(() => ({ x: 0, y: 0 }));
+
+  const visited = new Set<string>();
+  visited.add(`${knots[9].x}:${knots[9].y}`);
+
+  // display(knots, { x: 80, y: 40 }, { x: 11, y: 5 });
+  for (const [direction, steps] of moves) {
+    for (let i = 0; i < steps; i++) {
+      // Move head
+      moveInDirection(knots[0], direction);
+
+      // Tails should follow the knot in front of them
+      for (let j = 1; j < knots.length; j++) {
+        const head = knots[j - 1];
+        const tail = knots[j];
+        followHead(head, tail, direction);
+      }
+      visited.add(`${knots[9].x}:${knots[9].y}`);
+    }
+
+    // console.log(`== ${direction} ${steps} ==`);
+    // display(knots, { x: 80, y: 40 }, { x: 11, y: 5 });
   }
 
   return visited.size;
--- 2022/09/test.ts
+++ 2022/09/test.ts
@@ -1,7 +1,6 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
-
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `R 4
@@ -13,4 +12,28 @@ D 1
 L 5
 R 2`;
   assertEquals(solvePart1(new Input(input)), 13);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2`;
+  assertEquals(solvePart2(new Input(input)), 1);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20`;
+  assertEquals(solvePart2(new Input(input)), 36);
 });
