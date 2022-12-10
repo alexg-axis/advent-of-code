e788f795ad1f8f6f7fa400893da3ae68d2f2e604
Alex Gustafsson <alexg@axis.com>
2022-12-10 07:49:33 +0100

    Add solution to 2022/12/10 part 2

--- 2022/10/solutions.ts
+++ 2022/10/solutions.ts
@@ -3,6 +3,14 @@ import { sum } from "../../utils/deno/arrays.ts";
 
 type Instruction = "addx" | "noop";
 
+function replaceAt(string: string, index: number, replacement: string): string {
+  return (
+    string.substring(0, index) +
+    replacement +
+    string.substring(index + replacement.length)
+  );
+}
+
 export function solvePart1(input: Input): number {
   const instructions: [Instruction, number][] = input.lines.map((x) => {
     const parts = x.split(" ");
@@ -26,4 +34,35 @@ export function solvePart1(input: Input): number {
   }
 
   return signalStrengths.reduce(sum);
+}
+
+export function solvePart2(input: Input): string {
+  const instructions: [Instruction, number][] = input.lines.map((x) => {
+    const parts = x.split(" ");
+    return [parts[0] as Instruction, Number(parts[1])];
+  });
+
+  let crt = ".".repeat(6 * 40);
+
+  let cycle = 1;
+  let x = 1;
+
+  for (const [instruction, arg] of instructions) {
+    const cycles = instruction === "noop" ? 1 : 2;
+    const toAdd = instruction === "noop" ? 0 : arg;
+    for (let i = 0; i < cycles; i++, cycle++) {
+      crt = replaceAt(
+        crt,
+        cycle - 1,
+        (cycle - 1) % 40 === x - 1 ||
+          (cycle - 1) % 40 === x ||
+          (cycle - 1) % 40 === x + 1
+          ? "#"
+          : "."
+      );
+    }
+    x += toAdd;
+  }
+
+  return crt.match(/.{40}/g)!.join("\n");
 }
