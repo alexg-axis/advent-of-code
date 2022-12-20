a43a3ff06024cb4c0ac0f91e3df01356200682ac
Alex Gustafsson <alexg@axis.com>
2022-12-20 10:36:50 +0100

    Add solution to 2022/12/20 part 2

--- 2022/20/solutions.ts
+++ 2022/20/solutions.ts
@@ -4,32 +4,58 @@ import { Input } from "../../utils/deno/input.ts";
 type LinkedList<T> = Node<T>[];
 type Node<T> = { x: T; next: Node<T> | null };
 
-export function solvePart1(input: Input): number {
+function createList(input: Input): LinkedList<number> {
   const list: LinkedList<number> = input.lines
     .map(Number)
     .map((x) => ({ x, next: null }));
   for (let i = 0; i < list.length - 1; i++) {
     list[i].next = list[i + 1];
   }
+  return list;
+}
 
-  let current: Node<number> | null = list[0];
+function mix(list: LinkedList<number>, start: Node<number>) {
+  let current: Node<number> | null = start;
   while (current) {
     const position = list.indexOf(current);
     // Wrapping does not consume a step
     let newPosition = position + current.x;
-    if (newPosition < 0) newPosition = newPosition + list.length - 1;
+    if (newPosition < 0)
+      newPosition = (newPosition % (list.length - 1)) + list.length - 1;
     if (newPosition >= list.length)
       newPosition = newPosition % (list.length - 1);
     list.splice(position, 1);
     list.splice(newPosition, 0, current);
     current = current.next;
   }
+}
 
+function grove(list: LinkedList<number>): number {
   const zero = list.findIndex((x) => x.x === 0);
-  const result = [1000, 2000, 3000]
+  return [1000, 2000, 3000]
     .map((x) => (zero + x) % list.length)
     .map((x) => list[x].x)
     .reduce(sum);
+}
 
-  return result;
+export function solvePart1(input: Input): number {
+  const list = createList(input);
+
+  mix(list, list[0]);
+
+  return grove(list);
+}
+
+export function solvePart2(input: Input): number {
+  const list = createList(input);
+  for (const x of list) {
+    x.x *= 811589153;
+  }
+
+  const start = list[0];
+  for (let i = 0; i < 10; i++) {
+    mix(list, start);
+  }
+
+  return grove(list);
 }
--- 2022/20/test.ts
+++ 2022/20/test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `1
@@ -11,4 +11,15 @@ Deno.test("part 1 - given test case", () => {
 0
 4`;
   assertEquals(solvePart1(new Input(input)), 3);
+});
+
+Deno.test("part 1 - given test case", () => {
+  const input = `1
+2
+-3
+3
+-2
+0
+4`;
+  assertEquals(solvePart2(new Input(input)), 1623178306);
 });
