7f88ebdbaa483a3045b79a39184c25900e69cf4f
Alex Gustafsson <alexg@axis.com>
2023-12-07 08:37:16 +0100

    Add solution to 2023/12/07 part 2

--- 2023/07/solutions.ts
+++ 2023/07/solutions.ts
@@ -1,4 +1,5 @@
 import { Input } from "../../utils/deno/input.ts";
+import { combinationsWithReplacement } from "https://deno.land/x/combinatorics@1.0.1/combinations_with_replacement.ts";
 
 type Card =
   | "A"
@@ -15,11 +16,65 @@ type Card =
   | "3"
   | "2";
 
-const Cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
+const CardsRule1 = [
+  "A",
+  "K",
+  "Q",
+  "J",
+  "T",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+];
+const CardsRule2 = [
+  "A",
+  "K",
+  "Q",
+  "T",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "J",
+];
 
 type Hand = { cards: Card[]; bid: number };
 
-function calculateStrength(hand: Hand): number {
+function calculateStrength(hand: Hand, joker: boolean): number {
+  if (joker && hand.cards.includes("J")) {
+    let maxStrength = 0;
+    const indexes: number[] = [];
+    for (let i = 0; i < hand.cards.length; i++) {
+      if (hand.cards[i] === "J") {
+        indexes.push(i);
+      }
+    }
+
+    for (const x of combinationsWithReplacement(CardsRule2, indexes.length)) {
+      const newCards = [...hand.cards];
+      for (let i = 0; i < indexes.length; i++) {
+        newCards[indexes[i]] = x[i] as Card;
+      }
+      const strength = calculateStrength({ cards: newCards, bid: 0 }, false);
+      if (strength === 7) {
+        return 7;
+      }
+      if (strength > maxStrength) {
+        maxStrength = strength;
+      }
+    }
+    return maxStrength;
+  }
+
   const unique = Array.from(new Set(hand.cards));
   // Five of a type
   if (unique.length === 1) {
@@ -70,21 +125,24 @@ function calculateStrength(hand: Hand): number {
   return 2;
 }
 
-function sortByStrength(a: Hand, b: Hand): number {
-  const aStrength = calculateStrength(a);
-  const bStrength = calculateStrength(b);
-  const diff = bStrength - aStrength;
-  if (diff === 0) {
-    for (let i = 0; i < a.cards.length; i++) {
-      const aIndex = Cards.indexOf(a.cards[i]);
-      const bIndex = Cards.indexOf(b.cards[i]);
-      if (aIndex !== bIndex) {
-        return aIndex - bIndex;
+function sortByStrength(joker: boolean): (a: Hand, b: Hand) => number {
+  const rule = joker ? CardsRule2 : CardsRule1;
+  return (a: Hand, b: Hand) => {
+    const aStrength = calculateStrength(a, joker);
+    const bStrength = calculateStrength(b, joker);
+    const diff = bStrength - aStrength;
+    if (diff === 0) {
+      for (let i = 0; i < a.cards.length; i++) {
+        const aIndex = rule.indexOf(a.cards[i]);
+        const bIndex = rule.indexOf(b.cards[i]);
+        if (aIndex !== bIndex) {
+          return aIndex - bIndex;
+        }
       }
     }
-  }
 
-  return diff;
+    return diff;
+  };
 }
 
 export function solvePart1(input: Input): number {
@@ -93,7 +151,26 @@ export function solvePart1(input: Input): number {
     return { cards: cards.split("") as Card[], bid: Number(bid) };
   });
 
-  hands.sort(sortByStrength);
+  hands.sort(sortByStrength(false));
+
+  // console.log(hands.map((x) => ({ ...x, strength: calculateStrength(x) })));
+
+  let score = 0;
+  for (let i = 0; i < hands.length; i++) {
+    const rank = hands.length - i;
+    score += hands[i].bid * rank;
+  }
+
+  return score;
+}
+
+export function solvePart2(input: Input): number {
+  const hands: Hand[] = input.lines.map((x) => {
+    const [cards, bid] = x.split(" ");
+    return { cards: cards.split("") as Card[], bid: Number(bid) };
+  });
+
+  hands.sort(sortByStrength(true));
 
   // console.log(hands.map((x) => ({ ...x, strength: calculateStrength(x) })));
 
--- 2023/07/test.ts
+++ 2023/07/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `32T3K 765
@@ -11,4 +11,14 @@ KTJJT 220
 QQQJA 483`;
 
   assertEquals(solvePart1(new Input(input)), 6440);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`;
+
+  assertEquals(solvePart2(new Input(input)), 5905);
 });
