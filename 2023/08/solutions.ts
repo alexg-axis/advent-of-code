ca2431d6cd3628d6827249f4f0b8542b1dcd0110
Alex Gustafsson <alexg@axis.com>
2023-12-08 09:47:31 +0100

    Add solution to 2023/12/08 part 2

--- 2023/08/solutions.ts
+++ 2023/08/solutions.ts
@@ -21,17 +21,48 @@ function parseGraph(input: Input): Graph {
   return { moves, nodes };
 }
 
-export function solvePart1(input: Input): number {
-  const graph = parseGraph(input);
-
-  let currentNode = "AAA";
-  for (let i = 0; i < 1e10; i++) {
+function moves(graph: Graph, start: string, end: string): number {
+  let currentNode = start;
+  for (let i = 0; i < 20000; i++) {
     const move = graph.moves[i % graph.moves.length];
     currentNode = graph.nodes[currentNode][move === "L" ? 0 : 1];
-    if (currentNode === "ZZZ") {
+    if (currentNode === end) {
       return i + 1;
     }
   }
-
   return -1;
+}
+
+export function solvePart1(input: Input): number {
+  const graph = parseGraph(input);
+
+  return moves(graph, "AAA", "ZZZ");
+}
+
+function gcd(a: number, b: number): number {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return a;
+}
+
+function lcm(...values: number[]): number {
+  if (values.length === 2) {
+    return (values[0] * values[1]) / gcd(values[0], values[1]);
+  } else {
+    return lcm(values[0], lcm(...values.slice(1)));
+  }
+}
+
+export function solvePart2(input: Input): number {
+  const graph = parseGraph(input);
+
+  const startNodes = Object.keys(graph.nodes).filter((x) => x.endsWith("A"));
+  const goalNodes = Object.keys(graph.nodes).filter((x) => x.endsWith("Z"));
+
+  const steps = startNodes
+    .map((x) => goalNodes.map((y) => moves(graph, x, y)))
+    .map((x) => x.find((x) => x !== -1)!);
+
+  return lcm(...steps);
 }
--- 2023/08/test.ts
+++ 2023/08/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `RL
@@ -25,4 +25,19 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`;
 
   assertEquals(solvePart1(new Input(input)), 6);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+
+  assertEquals(solvePart2(new Input(input)), 6);
 });
