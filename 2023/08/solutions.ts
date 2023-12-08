90f72a593fc15014296f91f712a517f82e35a9ad
Alex Gustafsson <alexg@axis.com>
2023-12-08 08:44:46 +0100

    Remove unnecessary cache

--- 2023/08/solutions.ts
+++ 2023/08/solutions.ts
@@ -24,43 +24,12 @@ function parseGraph(input: Input): Graph {
 export function solvePart1(input: Input): number {
   const graph = parseGraph(input);
 
-  let visited: string[] = [];
-  let moves: string[] = [];
-  const visitedVisited: { i: number; moves: string[]; visited: string[] }[] =
-    [];
-  const patterns: Record<string, number> = {};
   let currentNode = "AAA";
   for (let i = 0; i < 1e10; i++) {
-    if (currentNode === "AAA") {
-      for (const possible of Object.keys(patterns)) {
-        let pattern = "";
-        for (let j = 0; j < graph.moves.length; j++) {
-          pattern += graph.moves[(j + i) % graph.moves.length];
-          if (pattern === possible) {
-            i += patterns[pattern];
-            currentNode = "AAA";
-            visited = [];
-            moves = [];
-            break;
-          }
-        }
-      }
-    }
-
     const move = graph.moves[i % graph.moves.length];
     currentNode = graph.nodes[currentNode][move === "L" ? 0 : 1];
     if (currentNode === "ZZZ") {
       return i + 1;
-    }
-
-    visited.push(currentNode);
-    moves.push(move);
-    if (currentNode === "AAA") {
-      visitedVisited.push({ i, visited, moves });
-      // console.log(i, moves, visited);
-      patterns[moves.join("")] = visited.length;
-      moves = [];
-      visited = [];
     }
   }
 
