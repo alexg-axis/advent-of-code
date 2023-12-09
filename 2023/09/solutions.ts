a0492371bfbe45500b4318b7e938b65fc1194f95
Alex Gustafsson <alexg@axis.com>
2023-12-09 07:45:06 +0100

    Add solution to 2023/12/09 part 2

--- 2023/09/solutions.ts
+++ 2023/09/solutions.ts
@@ -29,3 +29,33 @@ export function solvePart1(input: Input): number {
 
   return addedSum;
 }
+
+export function solvePart2(input: Input): number {
+  const history = input.lines.map((x) => x.split(/ +/).map(Number));
+  let addedSum = 0;
+  for (let i = 0; i < history.length; i++) {
+    let current = [...history[i]];
+    const first: number[] = [];
+    for (let j = 0; j < history[i].length; j++) {
+      const sum = current.reduce((sum, x) => sum + x, 0);
+      if (sum === 0) {
+        first.push(0);
+        for (let k = first.length - 2; k >= 0; k--) {
+          const value = -(first[k + 1] - first[k]);
+          first[k] = value;
+        }
+        addedSum += first[0];
+        break;
+      }
+
+      first.push(current[0]);
+      const next: number[] = [];
+      for (let k = 1; k < current.length; k++) {
+        next.push(current[k] - current[k - 1]);
+      }
+      current = next;
+    }
+  }
+
+  return addedSum;
+}
--- 2023/09/test.ts
+++ 2023/09/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `0 3 6 9 12 15
@@ -9,4 +9,12 @@ Deno.test("part 1 - given test case", () => {
 10 13 16 21 30 45`;
 
   assertEquals(solvePart1(new Input(input)), 114);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`;
+
+  assertEquals(solvePart2(new Input(input)), 2);
 });
