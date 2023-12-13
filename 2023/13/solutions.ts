b17de39f56f7f0fcfc4dfbe9c134fdd2a5991409
Alex Gustafsson <alexg@axis.com>
2023-12-13 11:03:39 +0100

    Add solution to 2023/12/13 part 2

--- 2023/13/solutions.ts
+++ 2023/13/solutions.ts
@@ -6,6 +6,50 @@ import { Input } from "../../utils/deno/input.ts";
  */
 type Cell = "." | "#";
 
+function findMirror(map: Cell[][]): [number, number][] {
+  const results: [number, number][] = [];
+
+  let row = -1;
+  rows: for (let i = 0; i < map.length - 1; i++) {
+    const currentRow = map[i].join("");
+    if (currentRow !== map[i + 1].join("")) {
+      continue rows;
+    }
+
+    for (let j = 0; j < i && i + j + 2 < map.length; j++) {
+      const above = map[i - j - 1];
+      const below = map[i + j + 2];
+      if (above.join("") !== below.join("")) {
+        continue rows;
+      }
+    }
+
+    row = i;
+    results.push([-1, row]);
+  }
+
+  let column = -1;
+  columns: for (let i = 0; i < map[0].length - 1; i++) {
+    const currentColumn = map.map((x) => x[i]).join("");
+    if (currentColumn !== map.map((x) => x[i + 1]).join("")) {
+      continue columns;
+    }
+
+    for (let j = 0; j < i && i + j + 2 < map[0].length; j++) {
+      const left = map.map((x) => x[i - j - 1]);
+      const right = map.map((x) => x[i + j + 2]);
+      if (left.join("") !== right.join("")) {
+        continue columns;
+      }
+    }
+
+    column = i;
+    results.push([column, -1]);
+  }
+
+  return results;
+}
+
 export function solvePart1(input: Input): number {
   const maps = input.raw
     .trim()
@@ -14,50 +58,53 @@ export function solvePart1(input: Input): number {
 
   let sum = 0;
   for (const map of maps) {
-    let row = -1;
-    rows: for (let i = 0; i < map.length - 1; i++) {
-      const currentRow = map[i].join("");
-      if (currentRow !== map[i + 1].join("")) {
-        continue rows;
-      }
-
-      for (let j = 0; j < i && i + j + 2 < map.length; j++) {
-        const up = map[i - j - 1];
-        const down = map[i + j + 2];
-        if (up.join("") !== down.join("")) {
-          continue rows;
-        }
-      }
-
-      row = i;
-      break;
-    }
-    if (row !== -1) {
-      sum += (row + 1) * 100;
-      continue;
-    }
-
-    let column = -1;
-    columns: for (let i = 0; i < map[0].length - 1; i++) {
-      const currentColumn = map.map((x) => x[i]).join("");
-      if (currentColumn !== map.map((x) => x[i + 1]).join("")) {
-        continue columns;
-      }
-
-      for (let j = 0; j < i && i + j + 2 < map[0].length; j++) {
-        const left = map.map((x) => x[i - j - 1]);
-        const right = map.map((x) => x[i + j + 2]);
-        if (left.join("") !== right.join("")) {
-          continue columns;
-        }
-      }
-
-      column = i;
-      break;
-    }
+    const [column, row] = findMirror(map)[0];
     if (column !== -1) {
       sum += column + 1;
-      continue;
+    } else if (row !== -1) {
+      sum += (row + 1) * 100;
+    } else {
+      throw new Error("no solution found");
+    }
+  }
+
+  return sum;
+}
+
+export function solvePart2(input: Input): number {
+  const maps = input.raw
+    .trim()
+    .split(/\n\n/)
+    .map((x) => x.split("\n").map((x) => x.split("") as Cell[]));
+
+  let sum = 0;
+  for (const map of maps) {
+    const [originalColumn, originalRow] = findMirror(map)[0];
+
+    const ash = map
+      .map((row, y) => row.map((column, x) => (column === "." ? [x, y] : [])))
+      .flat()
+      .filter((x) => x.length > 0)
+      .map((x) => [...x, "#"] as [number, number, Cell]);
+    const rocks = map
+      .map((row, y) => row.map((column, x) => (column === "#" ? [x, y] : [])))
+      .flat()
+      .filter((x) => x.length > 0)
+      .map((x) => [...x, "."] as [number, number, Cell]);
+    const replacements = [...ash, ...rocks];
+
+    replacements: for (const [x, y, target] of replacements) {
+      const replacedMap = [...map.map((x) => [...x])];
+      replacedMap[y][x] = target;
+      for (const [column, row] of findMirror(replacedMap)) {
+        if (column !== -1 && column !== originalColumn) {
+          sum += column + 1;
+          break replacements;
+        } else if (row !== -1 && row !== originalRow) {
+          sum += (row + 1) * 100;
+          break replacements;
+        }
+      }
     }
   }
 
--- 2023/13/test.ts
+++ 2023/13/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `#.##..##.
@@ -21,4 +21,129 @@ Deno.test("part 1 - given test case", () => {
 #....#..#`;
 
   assertEquals(solvePart1(new Input(input)), 405);
+});
+
+Deno.test("part 1 - input test case", () => {
+  const input = `....#.#..
+.######.#
+##.......
+###.....#
+.#.#.#.#.
+##.#.#.#.
+###.....#
+##.......
+.######.#
+....#.#..
+....#.#..`;
+
+  assertEquals(solvePart1(new Input(input)), 1000);
+});
+
+Deno.test("part 1 - input test case", () => {
+  const input = `
+#####.#
+......#
+.##.#.#
+.....##
+.##.###
+#..#..#
+.##....
+.####..
+####.##
+.##.#..
+.....#.
+#..##..
+######.
+.....#.
+#..#..#
+#..#..#
+.....#.
+
+`;
+
+  assertEquals(solvePart1(new Input(input)), 1500);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#`;
+
+  assertEquals(solvePart2(new Input(input)), 400);
+});
+
+Deno.test("part 2 - input test case", () => {
+  const input = `....#.#..
+.######.#
+##.......
+###.....#
+.#.#.#.#.
+##.#.#.#.
+###.....#
+##.......
+.######.#
+....#.#..
+....#.#..`;
+
+  assertEquals(solvePart2(new Input(input)), 500);
+});
+
+Deno.test("part 2 - input test case", () => {
+  const input = `
+#####.#
+......#
+.##.#.#
+.....##
+.##.###
+#..#..#
+.##....
+.####..
+####.##
+.##.#..
+.....#.
+#..##..
+######.
+.....#.
+#..#..#
+#..#..#
+.....#.
+
+`;
+
+  assertEquals(solvePart2(new Input(input)), 2);
+});
+
+Deno.test("part 2 - input test case", () => {
+  const input = `##.#.#..####.
+..##.....#..#
+..##.....#..#
+##.#.#..####.
+.#.######..##
+.#.#.#...#.#.
+###.#.#####.#
+#..##.#.##...
+.##.#####.###
+#.##.####.#.#
+#.##.##.#.#.#
+.##.#####.###
+#..##.#.##...
+###.#.#####.#
+.#.#.#...#.#.
+.#.######..##
+##.#.#..####.
+`;
+
+  assertEquals(solvePart2(new Input(input)), 1000);
 });
