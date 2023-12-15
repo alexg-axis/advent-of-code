5ba09484748450631445b712714cf5b356b38b18
Alex Gustafsson <alexg@axis.com>
2023-12-15 08:35:03 +0100

    Add solution to 2023/12/15 part 2

--- 2023/15/solutions.ts
+++ 2023/15/solutions.ts
@@ -17,3 +17,73 @@ export function solvePart1(input: Input): number {
     .map(hash)
     .reduce((sum, x) => sum + x);
 }
+
+type Instruction =
+  | {
+      label: string;
+      operation: "-";
+    }
+  | { label: string; operation: "="; value: number };
+
+function parseInstruction(instruction: string): Instruction {
+  const [_, label, operation, value] = instruction.match(/(\w+)([=-])(\d+)?/)!;
+  if (operation === "-") {
+    return { label, operation };
+  } else if (operation === "=") {
+    return { label, operation, value: Number(value) };
+  } else {
+    throw new Error("unreachable");
+  }
+}
+
+export function solvePart2(input: Input): number {
+  const boxes: Record<string, number>[] = new Array(256)
+    .fill(null)
+    .map(() => ({}));
+
+  const instructions = input.raw
+    .replaceAll("\n", "")
+    .split(",")
+    .map(parseInstruction);
+
+  for (const instruction of instructions) {
+    // console.log(instruction);
+    const index = hash(instruction.label);
+    if (instruction.operation === "=") {
+      // Maps in JS have keys ordered by insertion, no need to keep track of internal order
+      boxes[index][instruction.label] = instruction.value;
+    } else {
+      delete boxes[index][instruction.label];
+    }
+    // console.log(
+    //   boxes
+    //     .map((x) =>
+    //       Object.values(x).length === 0
+    //         ? "{}"
+    //         : JSON.stringify(Object.entries(x))
+    //     )
+    //     .join("\n")
+    // );
+  }
+
+  // console.log(
+  //   boxes
+  //     .map((x) =>
+  //       Object.values(x).length === 0 ? "{}" : JSON.stringify(Object.entries(x))
+  //     )
+  //     .join("\n")
+  // );
+
+  // - One plus the box number of the lens in question.
+  // - The slot number of the lens within the box: `1` for the first lens, `2` for the second lens, and so on.
+  // - The focal length of the lens.
+  let sum = 0;
+  for (let i = 0; i < boxes.length; i++) {
+    const values = Object.values(boxes[i]);
+    for (let j = 0; j < values.length; j++) {
+      sum += (1 + i) * (j + 1) * values[j];
+    }
+  }
+
+  return sum;
+}
--- 2023/15/test.ts
+++ 2023/15/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `HASH`;
@@ -13,4 +13,10 @@ Deno.test("part 1 - given test case", () => {
   const input = `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`;
 
   assertEquals(solvePart1(new Input(input)), 1320);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`;
+
+  assertEquals(solvePart2(new Input(input)), 145);
 });
