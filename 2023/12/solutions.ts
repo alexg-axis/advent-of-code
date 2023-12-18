9a7d8ed46d33a8ebb246852edc55369920af844c
Alex Gustafsson <alexg@axis.com>
2023-12-18 21:59:13 +0100

    2023/12 BFS

--- 2023/12/solutions.ts
+++ 2023/12/solutions.ts
@@ -1,89 +1,96 @@
-import { permutations } from "https://deno.land/x/combinatorics@1.0.1/permutations.ts";
 import { Input } from "../../utils/deno/input.ts";
-import { combinations } from "https://deno.land/x/combinatorics@1.0.1/combinations.ts";
-
-/**
- * - `.` operational.
- * - `#` damaged.
- * - `?` unknown.
- */
-type Cell = "." | "#" | "?";
+import { combinationsWithReplacement } from "https://deno.land/x/combinatorics@1.0.1/combinations_with_replacement.ts";
 
 interface Report {
-  cells: Cell[];
+  cells: string;
   runs: number[];
 }
 
 function parseReport(line: string): Report {
   const [cells, runs] = line.split(/ /g);
   return {
-    cells: cells.split("") as Cell[],
+    cells,
     runs: runs.split(",").map(Number),
   };
 }
 
-function isValid(cells: Cell[], runs: number[]): boolean {
-  const currentRuns: number[] = [];
-  let runStart = -1;
+function expand(report: Report): Report {
+  return {
+    cells: new Array(5).fill(report.cells).join("?"),
+    runs: new Array(5).fill(report.runs).flat(),
+  };
+}
+
+function isPossible(cells: string, runs: number[]): boolean {
+  cells += ".";
+  let run = 0;
+  let passed = 0;
   for (let i = 0; i < cells.length; i++) {
-    if (runStart === -1 && cells[i] === "#") {
-      runStart = i;
-    } else if (runStart > -1 && cells[i] === ".") {
-      currentRuns.push(i - runStart);
-      runStart = -1;
+    if (cells[i] === "#") {
+      passed++;
+    } else if (cells[i] === ".") {
+      if (passed > 0) {
+        if (runs[run] !== passed) {
+          // console.log("run mismatch", passed, run, runs[run]);
+          return false;
+        }
+        run++;
+        passed = 0;
+      }
+    } else {
+      if (passed > runs[run]) {
+        // console.log("run overrun");
+        return false;
+      }
+      // ? - benefit of a doubt - could be possible
+      return true;
     }
   }
-  if (runStart > -1) {
-    currentRuns.push(cells.length - runStart);
+
+  return run === runs.length;
+}
+
+function clean(cells: string): string {
+  return cells.replace(/\.\.+/, ".").replace(/^\.+|\.+$/, "");
+}
+
+function solve(report: Report): number {
+  const queue = [clean(report.cells)];
+  let possible = 0;
+  while (queue.length > 0) {
+    const part = queue.shift()!;
+    if (!isPossible(part, report.runs)) {
+      // console.log(part, report.runs, "not possible");
+      continue;
+    }
+    // console.log(part, report.runs, "possible");
+    // console.log(part);
+
+    const index = part.indexOf("?");
+    if (index === -1) {
+      possible++;
+    } else {
+      // console.log(part);
+      // console.log(part.substring(0, index) + "." + part.substring(index + 1));
+      // console.log(part.substring(0, index) + "#" + part.substring(index + 1));
+      // console.log();
+      queue.push(
+        clean(part.substring(0, index) + "." + part.substring(index + 1)),
+        clean(part.substring(0, index) + "#" + part.substring(index + 1))
+      );
+    }
   }
-  return currentRuns.join(",") === runs.join(",");
+  // console.log(possible);
+  // console.log();
+  return possible;
 }
 
 export function solvePart1(input: Input): number {
   const reports = input.lines.map(parseReport);
+  return reports.map(solve).reduce((sum, x) => sum + x);
+}
 
-  let possibilites = 0;
-  for (const report of reports) {
-    const missingIndexes = new Array(report.cells.length)
-      .fill(0)
-      .map((_, i) => i)
-      .filter((i) => report.cells[i] === "?");
-    // All broken
-    if (
-      isValid(
-        report.cells.map((x) => (x === "?" ? "#" : x)),
-        report.runs
-      )
-    ) {
-      possibilites++;
-    }
-    // All operational
-    if (
-      isValid(
-        report.cells.map((x) => (x === "?" ? "." : x)),
-        report.runs
-      )
-    ) {
-      possibilites++;
-    }
-    // All other combinations
-    for (let i = 1; i < missingIndexes.length; i++) {
-      for (const combination of combinations(missingIndexes, i)) {
-        const replaced = [...report.cells];
-        for (let j = 0; j < combination.length; j++) {
-          replaced[combination[j]] = ".";
-        }
-        for (let j = 0; j < replaced.length; j++) {
-          if (replaced[j] === "?") {
-            replaced[j] = "#";
-          }
-        }
-        if (isValid(replaced, report.runs)) {
-          possibilites++;
-        }
-      }
-    }
-  }
-
-  return possibilites;
+export function solvePart2(input: Input): number {
+  const reports = input.lines.map(parseReport).map(expand);
+  return reports.map(solve).reduce((sum, x) => sum + x);
 }
--- 2023/12/test.ts
+++ 2023/12/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `???.### 1,1,3
@@ -12,4 +12,94 @@ Deno.test("part 1 - given test case", () => {
 ?###???????? 3,2,1`;
 
   assertEquals(solvePart1(new Input(input)), 21);
+});
+
+Deno.test("part 1 - created test case", () => {
+  const input = `???.### 1,1,3`;
+
+  assertEquals(solvePart1(new Input(input)), 1);
+});
+
+Deno.test("part 1 - created test case", () => {
+  const input = `.??..??...?##. 1,1,3`;
+
+  assertEquals(solvePart1(new Input(input)), 4);
+});
+
+Deno.test("part 1 - created test case", () => {
+  const input = `?#?#?#?#?#?#?#? 1,3,1,6`;
+
+  assertEquals(solvePart1(new Input(input)), 1);
+});
+
+Deno.test("part 1 - created test case", () => {
+  const input = `????.#...#... 4,1,1`;
+
+  assertEquals(solvePart1(new Input(input)), 1);
+});
+
+Deno.test("part 1 - created test case", () => {
+  const input = `????.######..#####. 1,6,5`;
+
+  assertEquals(solvePart1(new Input(input)), 4);
+});
+
+Deno.test("part 1 - created test case", () => {
+  const input = `?###???????? 3,2,1`;
+
+  assertEquals(solvePart1(new Input(input)), 10);
+});
+
+// Deno.test("part 2 - given test case", () => {
+//   const input = `???.### 1,1,3
+// .??..??...?##. 1,1,3
+// ?#?#?#?#?#?#?#? 1,3,1,6
+// ????.#...#... 4,1,1
+// ????.######..#####. 1,6,5
+// ?###???????? 3,2,1`;
+
+//   assertEquals(solvePart2(new Input(input)), 525152);
+// });
+
+Deno.test("part 2 - created test case 1", () => {
+  const input = `???.### 1,1,3`;
+
+  assertEquals(solvePart2(new Input(input)), 1);
+});
+
+Deno.test("part 2 - created test case 2", () => {
+  // .??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.
+  const input = `.??..??...?##. 1,1,3`;
+
+  assertEquals(solvePart2(new Input(input)), 16384);
+});
+
+Deno.test("part 2 - created test case 3", () => {
+  const input = `?#?#?#?#?#?#?#? 1,3,1,6`;
+
+  assertEquals(solvePart2(new Input(input)), 1);
+});
+
+Deno.test("part 2 - created test case 4", () => {
+  const input = `????.#...#... 4,1,1`;
+
+  assertEquals(solvePart2(new Input(input)), 16);
+});
+
+Deno.test("part 2 - created test case 5", () => {
+  const input = `????.######..#####. 1,6,5`;
+
+  assertEquals(solvePart2(new Input(input)), 2500);
+});
+
+Deno.test("part 2 - created test case 6", () => {
+  const input = `?###???????? 3,2,1`;
+
+  assertEquals(solvePart2(new Input(input)), 506250);
+});
+
+Deno.test("part 2 - input test case", () => {
+  const input = `?#?.??.#?.??? 2,1,1,1`;
+
+  assertEquals(solvePart2(new Input(input)), -1); // TODO
 });
