eb8fa3bd4ecbf55591e600dfef26579d3ddecc81
Alex Gustafsson <alexg@axis.com>
2023-12-19 08:28:25 +0100

    Add solution to 2023/12/19 part 1

---
+++ 2023/19/solutions.ts
@@ -0,0 +1,170 @@
+import { Input } from "../../utils/deno/input.ts";
+
+// Default accept
+type Check =
+  | { type: "reject" }
+  | { type: "accept" }
+  | { type: "goto"; branch: string }
+  | {
+      type: "compare";
+      left: "x" | "m" | "a" | "s";
+      right: number;
+      operator: "<" | ">";
+      positiveBranch: string;
+    };
+
+interface Rating {
+  x: number;
+  m: number;
+  a: number;
+  s: number;
+}
+
+function parseInput(input: Input): {
+  workflows: Record<string, Check[]>;
+  ratings: Rating[];
+} {
+  const [workflows, ratings] = input.raw.trim().split(/\n\n/);
+  return {
+    workflows: Object.fromEntries(workflows.split("\n").map(parseWorkflow)),
+    ratings: ratings.split("\n").map(parseRating),
+  };
+}
+
+// vdm{m<3285:R,R}
+function parseWorkflow(workflow: string): [string, Check[]] {
+  const [name, part] = workflow.split("{");
+  return [name, part.slice(0, -1).split(",").map(parseCheck)];
+}
+
+// {x=656,m=803,a=13,s=1540}
+function parseRating(rating: string): Rating {
+  return Object.fromEntries(
+    rating
+      .slice(1, -1)
+      .split(",")
+      .map((x) => [x.split("=")[0], Number(x.split("=")[1])])
+  ) as unknown as Rating;
+}
+
+function parseCheck(check: string): Check {
+  if (check === "R") {
+    return { type: "reject" };
+  } else if (check === "A") {
+    return { type: "accept" };
+  } else {
+    const match = check.match(/(\w+)([<>])(\d+):(.*)/)!;
+    if (match) {
+      const [_, left, operator, right, positiveBranch] = match;
+      return {
+        type: "compare",
+        left: left as "x" | "m" | "a" | "s",
+        right: Number(right),
+        operator: operator as "<" | ">",
+        positiveBranch,
+      };
+    } else {
+      return { type: "goto", branch: check };
+    }
+  }
+}
+
+function solve(
+  instructions: { direction: "R" | "D" | "L" | "U"; steps: number }[]
+): number {
+  const coordinate = [0, 0];
+  const vertices: [number, number][] = [];
+
+  // total boundary area
+  let boundary = 0;
+  for (const { direction, steps } of instructions) {
+    const step = [0, 0];
+    switch (direction) {
+      case "U":
+        step[1] = -1;
+        break;
+      case "D":
+        step[1] = 1;
+        break;
+      case "L":
+        step[0] = -1;
+        break;
+      case "R":
+        step[0] = 1;
+        break;
+    }
+
+    coordinate[0] += step[0] * steps;
+    coordinate[1] += step[1] * steps;
+    vertices.push([...coordinate] as [number, number]);
+    boundary += steps;
+  }
+
+  // shoelace formula for area of interior points of polygon
+  vertices.push(vertices[0]);
+  let area = 0;
+  for (let i = 0; i < vertices.length - 1; i++) {
+    const [x1, y1] = vertices[i];
+    const [x2, y2] = vertices[i + 1];
+    area += (x2 + x1) * (y2 - y1);
+  }
+  const interior = Math.abs(area) / 2;
+
+  // Pick's theorem
+  return interior - boundary / 2 - 1 + boundary + 2;
+}
+
+function isAccepted(
+  ratings: Rating,
+  workflows: Record<string, Check[]>
+): boolean {
+  let current = "in";
+  for (let i = 0; i < 1e3; i++) {
+    if (current === "A") {
+      return true;
+    } else if (current === "R") {
+      return false;
+    }
+    if (!workflows[current]) {
+      throw new Error("unreachable");
+    }
+
+    const checks = workflows[current];
+    checkLoop: for (const check of checks) {
+      switch (check.type) {
+        case "accept":
+          return true;
+        case "reject":
+          return false;
+        case "goto":
+          current = check.branch;
+          break checkLoop;
+        case "compare":
+          switch (check.operator) {
+            case "<":
+              if (ratings[check.left] < check.right) {
+                current = check.positiveBranch;
+                break checkLoop;
+              }
+              break;
+            case ">":
+              if (ratings[check.left] > check.right) {
+                current = check.positiveBranch;
+                break checkLoop;
+              }
+              break;
+          }
+      }
+    }
+  }
+
+  throw new Error("unreachable");
+}
+
+export function solvePart1(input: Input): number {
+  const { workflows, ratings } = parseInput(input);
+
+  const accepted = ratings.filter((x) => isAccepted(x, workflows));
+
+  return accepted.reduce((sum, x) => sum + x.x + x.m + x.a + x.s, 0);
+}
