5bae28238933f05df2886b79bbbb4cbc914ad868
Alex Gustafsson <alexg@axis.com>
2023-12-22 22:04:51 +0100

    Add slooow solution to 2023/12/17 part 2

--- 2023/17/main.ts
+++ 2023/17/main.ts
@@ -1,4 +1,5 @@
 import input from "../../utils/deno/input.ts";
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
-console.log(solvePart1(input));
+// console.log(solvePart1(input));
+console.log(solvePart2(input));
--- 2023/17/solutions.ts
+++ 2023/17/solutions.ts
@@ -4,7 +4,7 @@ import { Input } from "../../utils/deno/input.ts";
 interface State {
   position: [number, number];
   direction: [number, number];
-  history: [number, number];
+  history: number;
 }
 
 const shifts = [
@@ -15,31 +15,27 @@ const shifts = [
 ];
 
 function stateKey(state: State): string {
-  return `${state.position.join(":")},${state.direction.join(
-    ":"
-  )},${state.history.join(":")}`;
+  return `${state.position.join(":")},${state.direction.join(":")},${
+    state.history
+  }`;
 }
 
 export function solvePart1(input: Input): number {
   const map = input.lines.map((x) => x.split("").map(Number));
 
-  const start: State = { position: [0, 0], direction: [1, 0], history: [0, 0] };
+  const start: State = { position: [0, 0], direction: [1, 0], history: 0 };
 
   const vertices = map
     .map((row, y) =>
       row.map((_, x) => {
         const result: State[] = [];
         for (const shift of shifts) {
-          for (let i = 0; i < 2; i++) {
-            for (const consecutive of [0, 1, 2, 3]) {
-              const history: [number, number] = [0, 0];
-              history[i] = consecutive;
-              result.push({
-                position: [x, y],
-                direction: [...shift] as [number, number],
-                history,
-              });
-            }
+          for (const consecutive of [0, 1, 2, 3]) {
+            result.push({
+              position: [x, y],
+              direction: [...shift] as [number, number],
+              history: consecutive,
+            });
           }
         }
         return result;
@@ -77,7 +73,7 @@ export function solvePart1(input: Input): number {
         continue;
       }
       // Illegal - too many straight moves
-      if (Math.abs(history[0] + dx) >= 3 || Math.abs(history[1] + dy) >= 3) {
+      if (dx === direction[0] && dy === direction[1] && history + 1 >= 3) {
         // console.log("  ", nx, ny, "too many straight moves");
         continue;
       }
@@ -94,10 +90,7 @@ export function solvePart1(input: Input): number {
         position: [nx, ny],
         direction: [dx, dy],
         // Empty history when turning
-        history:
-          direction[0] === dx && direction[1] === dy
-            ? [history[0] + dx, history[1] + dy]
-            : [0, 0],
+        history: direction[0] === dx && direction[1] === dy ? history + 1 : 0,
       };
       const nextKey = stateKey(next);
 
@@ -112,18 +105,14 @@ export function solvePart1(input: Input): number {
 
   const goals: string[] = [];
   for (const shift of shifts) {
-    for (let i = 0; i < 2; i++) {
-      for (const consecutive of [0, 1, 2, 3]) {
-        const history: [number, number] = [0, 0];
-        history[i] = consecutive;
-        goals.push(
-          stateKey({
-            position: [map[0].length - 1, map.length - 1],
-            direction: [...shift] as [number, number],
-            history,
-          })
-        );
-      }
+    for (const consecutive of [0, 1, 2, 3]) {
+      goals.push(
+        stateKey({
+          position: [map[0].length - 1, map.length - 1],
+          direction: [...shift] as [number, number],
+          history: consecutive,
+        })
+      );
     }
   }
 
@@ -135,6 +124,117 @@ export function solvePart1(input: Input): number {
     }
   }
 
-  // Random off by one for input.txt here...
+  return min;
+}
+
+export function solvePart2(input: Input): number {
+  const map = input.lines.map((x) => x.split("").map(Number));
+
+  const start: State = { position: [0, 0], direction: [1, 0], history: 0 };
+
+  const vertices = map
+    .map((row, y) =>
+      row.map((_, x) => {
+        const result: State[] = [];
+        for (const shift of shifts) {
+          for (const consecutive of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
+            result.push({
+              position: [x, y],
+              direction: [...shift] as [number, number],
+              history: consecutive,
+            });
+          }
+        }
+        return result;
+      })
+    )
+    .flat(2);
+
+  const distances = new Map(
+    vertices.map((state) => [stateKey(state), Number.MAX_VALUE])
+  );
+  distances.set(stateKey(start), 0);
+
+  const queue = new BinaryHeap<State>(
+    (a, b) => distances.get(stateKey(a))! - distances.get(stateKey(b))!
+  );
+  for (const vertice of vertices) {
+    queue.push(vertice);
+  }
+
+  while (queue.length > 0) {
+    const current = queue.pop()!;
+
+    const { position, direction, history } = current;
+
+    const [x, y] = position;
+
+    const currentCost = distances.get(stateKey(current))!;
+
+    for (const [dx, dy] of shifts) {
+      const nx = x + dx;
+      const ny = y + dy;
+      // Out of bounds
+      if (nx < 0 || nx >= map[0].length || ny < 0 || ny >= map.length) {
+        // console.log("  ", nx, ny, "out of bounds");
+        continue;
+      }
+      // Illegal - too many straight moves
+      if (dx === direction[0] && dy === direction[1] && history + 1 >= 10) {
+        // console.log("  ", nx, ny, "too many straight moves");
+        continue;
+      }
+      // Illegal - backwards
+      if (
+        (direction[0] !== 0 && direction[0] === -dx) ||
+        (direction[1] !== 0 && direction[1] === -dy)
+      ) {
+        // console.log("  ", nx, ny, "backwards");
+        continue;
+      }
+      // Illegal - too few in a row
+      if (dx !== direction[0] && dy !== direction[1] && history + 1 < 4) {
+        // console.log("  ", nx, ny, "not enough in a line");
+        continue;
+      }
+
+      const next: State = {
+        position: [nx, ny],
+        direction: [dx, dy],
+        // Empty history when turning
+        history: direction[0] === dx && direction[1] === dy ? history + 1 : 0,
+      };
+      const nextKey = stateKey(next);
+
+      const cost = currentCost + map[ny][nx];
+
+      if (cost < distances.get(nextKey)!) {
+        distances.set(nextKey, cost);
+        queue.push(next);
+      }
+    }
+  }
+
+  const goals: string[] = [];
+  for (const shift of shifts) {
+    for (const consecutive of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
+      goals.push(
+        stateKey({
+          position: [map[0].length - 1, map.length - 1],
+          direction: [...shift] as [number, number],
+          history: consecutive,
+        })
+      );
+    }
+  }
+
+  let min = Number.MAX_VALUE;
+  for (const goal of goals) {
+    const steps = distances.get(goal)!;
+    if (steps < min) {
+      min = steps;
+    }
+  }
+
   return min;
 }
