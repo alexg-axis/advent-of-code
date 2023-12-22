abb4138cefc078291f325aa4f290d785117720a6
Alex Gustafsson <alexg@axis.com>
2023-12-22 10:36:42 +0100

    Add solution to 2023/12/21 part 2

--- 2023/21/solutions.ts
+++ 2023/21/solutions.ts
@@ -1,4 +1,5 @@
 import { Input } from "../../utils/deno/input.ts";
+import { mod } from "../../utils/deno/math.ts";
 
 type Cell = "." | "#" | "S";
 
@@ -52,4 +53,83 @@ export function solvePart1(input: Input, steps = 64): number {
   }
 
   return currentQueue.length;
+}
+
+// Map is square. Start is in center, row and column is empty - aka will touch
+// edges, so there will be a diamond shape on the map.
+// Initial map radius is 65, diameter is 131. Next step is 130, diameter 196.
+// So map grows with 65 each edge, size is 131, so size grows quadratically:
+// x = 65 + 131n. If we find the first three values, we can interpolate to find
+// the answer
+// x1 = 65 (i = 64: 2722 for test, 3957 for input)
+// x2 = 196 (i = 195: 25621 for test, 35223 for input)
+// x3 = 261 (i = 260: 45473 for test, 97645 for input)
+// x4 = x1 + x2 + x3 + ?
+// ...
+// xn = x1 + x2 + x3 + ...
+// (26501365-65)/131 = 202300
+// So we're looking for x202300
+// Quadratic regression: y=ax2+bx+c
+//
+//
+// x202300 = 637537341306357
+export function solvePart2(input: Input, steps = 26501365): number {
+  const map = input.lines.map((x) => x.split("") as Cell[]);
+
+  const [startX, startY] = map
+    .map((row, y) => row.map((_, x) => [x, y]))
+    .flat()
+    .find(([x, y]) => map[y][x] === "S")!;
+
+  let currentQueue: [number, number][] = [[startX, startY]];
+  let nextQueue: [number, number][] = [];
+  let x1 = 0;
+  let x2 = 0;
+  let x3 = 0;
+  for (let i = 0; i < 261; i++) {
+    console.log(i);
+    const visited = new Set<string>();
+    nextQueue = [];
+    while (currentQueue.length > 0) {
+      const [x, y] = currentQueue.shift()!;
+      visited.add(`${x}:${y}`);
+
+      for (const [dx, dy] of [
+        [-1, 0],
+        [0, -1],
+        [1, 0],
+        [0, 1],
+      ]) {
+        const nextX = x + dx;
+        const nextY = y + dy;
+        const key = `${nextX}:${nextY}`;
+        if (
+          map[mod(nextY, map.length)][mod(nextX, map[0].length)] === "#" ||
+          visited.has(key) ||
+          nextQueue.find(([x, y]) => x === nextX && y === nextY)
+        ) {
+          continue;
+        }
+
+        nextQueue.push([nextX, nextY]);
+      }
+    }
+    currentQueue = nextQueue;
+
+    console.log("  ", nextQueue.length);
+    if (i === 64) {
+      x1 = nextQueue.length;
+    } else if (i === 195) {
+      x2 = nextQueue.length;
+    } else if (i === 260) {
+      x3 = nextQueue.length;
+    }
+  }
+
+  console.log(x1, x2, x3);
+
+  const fit = (n: number) =>
+    x1 + n * Math.floor(x2 - x1 + ((n - 1) * (x3 - x2 - x2 + x1)) / 2);
+
+  return fit((steps - 65) / 131);
 }
--- 2023/21/test.ts
+++ 2023/21/test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Input } from "../../utils/deno/input.ts";
 
-import { solvePart1 } from "./solutions.ts";
+import { solvePart1, solvePart2 } from "./solutions.ts";
 
 Deno.test("part 1 - given test case", () => {
   const input = `...........
@@ -17,4 +17,52 @@ Deno.test("part 1 - given test case", () => {
 ...........`;
 
   assertEquals(solvePart1(new Input(input), 6), 16);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........`;
+
+  assertEquals(solvePart2(new Input(input), 6), 16);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........`;
+
+  assertEquals(solvePart2(new Input(input), 1000), 668697);
+});
+
+Deno.test("part 2 - given test case", () => {
+  const input = `...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........`;
+
+  assertEquals(solvePart2(new Input(input), 5000), 16733044);
 });
